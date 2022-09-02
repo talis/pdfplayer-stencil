faf7d948dc37c1e60d78d5cb92d2c24bfcd7891a
Darren Arbon <35709034+darrenarbon@users.noreply.github.com>
2022-09-02 13:31:49 +0100

    remove gitignore

---
+++ dist/pdfplayer-stencil/pdfplayer-stencil.esm.js
@@ -0,0 +1,1 @@
+import{p as t,b as m}from"./p-92ac2897.js";(()=>{const m=import.meta.url,o={};return""!==m&&(o.resourcesUrl=new URL(".",m).href),t(o)})().then((t=>m([["p-819a3875",[[1,"my-component",{first:[1],middle:[1],last:[1]}]]]],t)));
